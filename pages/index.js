0ebf247fb42ea4e086963d619a12e013ac7c4d1a
Bishwas Niraula <bishwash.niraula@gmail.com>
2020-04-29 09:13:42 -0400

    updated
    updated

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,13 @@ import axios from "axios";
 import ProductList from '../components/Index/ProductList';
 import BaseUrl from '../utils/baseUrl';
 function Home({ products }) {
-  //console.log(products);
   return <ProductList products={products}/>
 }
 
 Home.getInitialProps = async () => {
-  // fetch data on server
   const url = `${BaseUrl}/api/products`;
   const response = await axios.get(url);
-  //console.log(response.data);
   return {products: response.data};
-  // return response data as an object
-  // note: this object will be merged with existing props
 };
 
 export default Home;
